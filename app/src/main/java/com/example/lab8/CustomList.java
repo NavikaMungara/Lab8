ed1bbd3d187cb7e818f85e272de41fcf6f723a21
Navika <navika@ualberta.ca>
2024-03-08 10:49:32 -0700

    implement method

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -62,11 +62,11 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void delete(City city){
-//        if (cities.contains(city)) {
-//            cities.remove(city);
-//        } else{
-//            throw new IllegalArgumentException();
-//        }
+        if (cities.contains(city)) {
+            cities.remove(city);
+        } else{
+            throw new IllegalArgumentException();
+        }
 
     }
 
