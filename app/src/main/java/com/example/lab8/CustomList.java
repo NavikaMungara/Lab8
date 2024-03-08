db7007718878921359b691e53b145ae4ca5351e2
Navika <navika@ualberta.ca>
2024-03-08 11:02:00 -0700

    implement hasCity method

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -71,12 +71,11 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public boolean hasCity(City city){
-//        if(cities.contains(city)){
-//            return true;
-//        } else {
-//            return false;
-//        }
-        return false;
+        if(cities.contains(city)){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public List<City> getCities() {
